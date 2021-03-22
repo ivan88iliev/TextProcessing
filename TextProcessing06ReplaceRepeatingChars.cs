b65897c86f8a265b55d9efc4fe6fa1ed3a159ac3
Ivan Iliev <73687077+ivan88iliev@users.noreply.github.com>
2021-03-23 01:00:57 +0200

    Add files via upload
    6.	 Replace Repeating Chars
    Write a program that reads a string from the console and replaces any sequence of the same letters with a single corresponding letter.

---
+++ TextProcessing06ReplaceRepeatingChars.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextProcessing06
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            //TextProcessing06ReplaceRepeatingChars
+
+            StringBuilder charBuilder = new StringBuilder();
+
+            string text = Console.ReadLine();
+            charBuilder.Append(text[0]);
+
+            for (int i = 1; i < text.Length; i++)
+            {
+                if (text[i] != charBuilder[charBuilder.Length-1])
+                {
+                    charBuilder.Append(text[i]);
+                }
+            }
+
+
+            Console.WriteLine(charBuilder);
+
+
+            //
+        }
+    }
+}
